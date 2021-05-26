5fc02e2d64d7f7eba02abb16f85a282af8116656
luojia65 <me@luojia.cc>
2021-05-26 22:08:08 +0800

    Small fix for entry stack

--- rustsbi-qemu/src/main.rs
+++ rustsbi-qemu/src/main.rs
@@ -125,30 +125,35 @@ fn print_misa_medeleg_mideleg() {
     println!("[rustsbi] medeleg: {:#x}", medeleg::read().bits());
 }
 
-const BOOT_STACK_SIZE: usize = 4096 * 4 * 8;
+const HART_STACK_SIZE: usize = 4 * 4096; // 16KiB
+const STACK_SIZE: usize = 8 * HART_STACK_SIZE; // assume 8 cores in QEMU
 
 #[link_section = ".bss.stack"]
-static mut BOOT_STACK: [u8; BOOT_STACK_SIZE] = [0; BOOT_STACK_SIZE];
+static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
 #[naked]
 #[link_section = ".text.entry"] 
 #[export_name = "_start"]
 unsafe extern "C" fn entry() -> ! {
-    asm!("
-    # 1. set sp
-    # sp = bootstack + (hartid + 1) * 0x10000
-    add     t0, a0, 1
-    slli    t0, t0, 14
-1:  auipc   sp, %pcrel_hi({boot_stack})
-    addi    sp, sp, %pcrel_lo(1b)
-    add     sp, sp, t0
-
-    # 2. jump to rust_main (absolute address)
+    asm!(
+    // 1. set sp
+    // sp = bootstack + (hartid + 1) * HART_STACK_SIZE
+    "
+    la      sp, {stack}
+    li      t0, {per_hart_stack_size}
+    addi    t1, a0, 1
+1:  add     sp, sp, t0
+    addi    t1, t1, -1
+    bnez    t1, 1b
+    ",
+    // 2. jump to rust_main (absolute address)
+    "
 1:  auipc   t0, %pcrel_hi({rust_main})
     addi    t0, t0, %pcrel_lo(1b)
     jr      t0
     ", 
-    boot_stack = sym BOOT_STACK, 
+    per_hart_stack_size = const HART_STACK_SIZE,
+    stack = sym STACK, 
     rust_main = sym rust_main,
     options(noreturn))
 }
