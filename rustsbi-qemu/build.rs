0c5cd6e6ff1018878ee3c7771a79f2236902f12c
luojia65 <me@luojia.cc>
2022-02-13 23:47:31 +0800

    rustsbi-qemu: include linker in build script as argument
    ...so that we don't need to copy and paster linker script files into target build folder
    
    Signed-off-by: luojia65 <me@luojia.cc>

--- rustsbi-qemu/build.rs
+++ rustsbi-qemu/build.rs
@@ -1,17 +1,4 @@
-use std::env;
-use std::fs;
-use std::io::Write;
-use std::path::PathBuf;
-
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/linker64.ld");
-
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    fs::File::create(out_dir.join("linker64.ld"))
-        .unwrap()
-        .write_all(include_bytes!("src/linker64.ld"))
-        .unwrap();
-    println!("cargo:rustc-link-search={}", out_dir.display());
+    println!("cargo:rustc-link-arg=-Trustsbi-qemu/src/linker64.ld");
 }
