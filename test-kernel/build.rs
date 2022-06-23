598e01175dae683f64e1db2842ef8cb0fab98b5c
YdrMaster <ydrml@hotmail.com>
2022-06-23 10:49:42 +0800

    refactor(xtask): 重写 xtask
    - 若 binutils 不存在，将自动安装
    - 自动处理文件扩展名、应用程序路径等问题以支持 windows 上开发
    
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- test-kernel/build.rs
+++ test-kernel/build.rs
@@ -1,20 +1,7 @@
-use std::{
-    env, fs,
-    io::{Result, Write},
-    path::PathBuf,
-};
-
-fn main() -> Result<()> {
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-
-    // Put the linker script somewhere the linker can find it
-    fs::File::create(out_dir.join("linker64.ld"))?.write_all(include_bytes!("src/linker64.ld"))?;
-    fs::File::create(out_dir.join("linker32.ld"))?.write_all(include_bytes!("src/linker32.ld"))?;
-    println!("cargo:rustc-link-search={}", out_dir.display());
-
+fn main() {
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/linker64.ld");
-    println!("cargo:rerun-if-changed=src/linker32.ld");
-
-    Ok(())
+    #[cfg(target_pointer_width = "64")]
+    println!("cargo:rustc-link-arg=-Ttest-kernel/src/linker64.ld");
+    #[cfg(target_pointer_width = "32")]
+    println!("cargo:rustc-link-arg=-Ttest-kernel/src/linker32.ld");
 }
